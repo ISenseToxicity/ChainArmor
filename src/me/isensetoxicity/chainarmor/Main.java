c3e8fb1cab44621f642654bb40b73351f84214ad
ISenseToxicity <david@davidvanderwulp.nl>
2021-09-09 00:47:48 +0200

    added Chain Support and config

--- src/me/isensetoxicity/chainarmor/Main.java
+++ src/me/isensetoxicity/chainarmor/Main.java
@@ -2,24 +2,37 @@ package me.isensetoxicity.chainarmor;
 
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    private final FileConfiguration config = this.getConfig();
 
         @Override
         public void onEnable() {
-            makeRecipe(Material.CHAINMAIL_HELMET,"chainMailHelmet","iiixiai");
-            makeRecipe(Material.CHAINMAIL_CHESTPLATE,"chainMailChestPlate","iaixiiixiii");
-            makeRecipe(Material.CHAINMAIL_LEGGINGS,"chainMailLeggings","iiixiaixiai");
-            makeRecipe(Material.CHAINMAIL_BOOTS, "chainMailBoots","iaixiai");
+            config.addDefault("IronBars",true);
+            config.addDefault("Chain",false);
+            config.options().copyDefaults(true);
+            this.saveConfig();
+            if(config.getBoolean("IronBars")&&!config.getBoolean("Chain")){
+                makeRecipe(Material.CHAINMAIL_HELMET,"chainMailHelmet","iiixiai",Material.IRON_BARS);
+                makeRecipe(Material.CHAINMAIL_CHESTPLATE,"chainMailChestPlate","iaixiiixiii",Material.IRON_BARS);
+                makeRecipe(Material.CHAINMAIL_LEGGINGS,"chainMailLeggings","iiixiaixiai",Material.IRON_BARS);
+                makeRecipe(Material.CHAINMAIL_BOOTS, "chainMailBoots","iaixiai",Material.IRON_BARS);
+            }else if(!config.getBoolean("IronBars")&&config.getBoolean("Chain")){
+                makeRecipe(Material.CHAINMAIL_HELMET,"chainMailHelmet","iiixiai",Material.CHAIN);
+                makeRecipe(Material.CHAINMAIL_CHESTPLATE,"chainMailChestPlate","iaixiiixiii",Material.CHAIN);
+                makeRecipe(Material.CHAINMAIL_LEGGINGS,"chainMailLeggings","iiixiaixiai",Material.CHAIN);
+                makeRecipe(Material.CHAINMAIL_BOOTS, "chainMailBoots","iaixiai",Material.CHAIN);
+            }
         }
 
         @Override
         public void onDisable() {
         }
-        public void makeRecipe(Material material,String key,String recipe){
+        public void makeRecipe(Material material,String key,String recipe,Material ingredientMateriel){
             ItemStack itemStack = new ItemStack(material);
             NamespacedKey namespacedKey = new NamespacedKey(this, key);
             ShapedRecipe shapedRecipe = new ShapedRecipe(namespacedKey,itemStack);
@@ -31,7 +44,7 @@ public class Main extends JavaPlugin {
             }else if(recipes.length == 3){
                 shapedRecipe.shape(recipes[0],recipes[1],recipes[2]);
             }
-            shapedRecipe.setIngredient('i',Material.IRON_BARS);
+            shapedRecipe.setIngredient('i',ingredientMateriel);
             getServer().addRecipe(shapedRecipe);
         }
     }
